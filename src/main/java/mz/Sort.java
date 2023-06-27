de04e02acf407ede088601985071bed2ca93ce49
Magyar Zoltán <118557170+MagyarZoli@users.noreply.github.com>
2023-06-27 21:33:45 +0200

    1.0 add SortFunctional Interface

--- src/main/java/mz/Sort.java
+++ src/main/java/mz/Sort.java
@@ -45,6 +45,28 @@ public interface Sort<T> {
     }
 
     /**
+     * {@link java.lang.FunctionalInterface @FunctionalInterface}: This annotation indicates that the interface is a functional interface.
+     * A functional interface is an interface that has only one abstract method and is used for lambda expressions or method references.
+     * This line defines the interface CompareTo with a generic type parameter {@code T} that extends the {@link java.lang.Comparable Comparable} interface.
+     * The Comparable interface is a built-in interface in Java that represents objects that can be compared to each other.
+     * @param       <T> when entering it, you can set which class type the interface is used as.
+     */
+    @FunctionalInterface
+    interface SortFunctional<T> {
+
+        /**
+         * Declares the abstract method {@code functionalCompareTo} within the interface.
+         * The method takes two parameters of type {@code T} (which extends {@link java.lang.Comparable Comparable})
+         * named {@code a} and {@code b} and returns a boolean value.
+         * This method is intended to compare two objects of type {@code T} and determine whether they satisfy a certain condition.
+         * @param       a first element to be examined.
+         * @param       b second element to be examined.
+         * @return      the returns a boolean value.
+         */
+        boolean functionalCompareTo(T a, T b);
+    }
+
+    /**
      * Every class inherited by polymorphism will contain the callable method. default pre-created method calls additional methods.
      * The {@code sortArray} method takes the {@code array} and the sorting {@code sequence} as parameters.
      * It then uses a switch statement to determine the value of sequence and perform the corresponding sorting operation.
