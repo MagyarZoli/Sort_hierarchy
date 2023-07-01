5e4608b86e60d0bc1164557bc97c5036ba263e10
Magyar Zoltán <118557170+MagyarZoli@users.noreply.github.com>
2023-07-01 16:55:53 +0200

    1.1 update IntoDPQ

--- src/main/java/mz/intro/introDPQ/IntroDPQ.java
+++ src/main/java/mz/intro/introDPQ/IntroDPQ.java
@@ -12,7 +12,7 @@ import mz.intro.Intro;
  * which guarantees worst-case <em>O(n log(n))</em> time complexity but has higher overhead.
  * Additionally, for small subarrays, IntroSort switches to Insertion Sort, which has good performance for small input sizes.
  * @param       <T> setting of a type based on which the elements can be sorted.
- * @since       1.0
+ * @since       1.1
  * @author      <a href=https://github.com/MagyarZoli>Magyar Zoltán</a>
  */
 @SuppressWarnings("rawtypes")
@@ -45,6 +45,11 @@ extends Intro<T> {
      * @param       left The starting index of the subarray to be sorted.
      * @param       right The ending index (inclusive) of the subarray to be sorted.
      * @param       maxDepth The maximum depth or recursion level allowed before switching to another sorting algorithm.
+     * @see         mz.intro.Intro#INTRO_SIZE
+     * @see         mz.intro.Intro#introSortClassInc(Comparable[], int, int)
+     * @see         mz.QuickInterface#partitionDualInc(Comparable[], int, int)
+     * @see         mz.intro.Intro#introRecursiveInc(Comparable[], int, int, int)
+     * @see         mz.InsertionInterface#insertionInc(Comparable[], int, int)
      */
     @Override
     default void introRecursiveInc(T[] array, int left, int right, int maxDepth) {
@@ -87,6 +92,11 @@ extends Intro<T> {
      * @param       left The starting index of the subarray to be sorted.
      * @param       right The ending index (inclusive) of the subarray to be sorted.
      * @param       maxDepth The maximum depth or recursion level allowed before switching to another sorting algorithm.
+     * @see         mz.intro.Intro#INTRO_SIZE
+     * @see         mz.intro.Intro#introSortClassDec(Comparable[], int, int)
+     * @see         mz.QuickInterface#partitionDualDec(Comparable[], int, int)
+     * @see         mz.intro.Intro#introRecursiveDec(Comparable[], int, int, int)
+     * @see         mz.InsertionInterface#insertionDec(Comparable[], int, int)
      */
     @Override
     default void introRecursiveDec(T[] array, int left, int right, int maxDepth) {
@@ -100,6 +110,61 @@ extends Intro<T> {
             introRecursiveDec(array, (pivots[1] + 1), right, (maxDepth - 1));
         } else {
             insertionDec(array, left, right);
+        }
+    }
+
+    /**
+     * {@code introRecursive} method is assumed to be a member of a class that overrides the interface method.
+     * It takes a parameter array of type {@code T[]},
+     * an integer {@code left} representing the left index, an integer {@code right} representing the right index,
+     * an integer {@code maxDepth} representing the maximum recursion depth,
+     * and an instance of the {@code SortFunctional<T>} interface as parameters.
+     * <ul>
+     *     <li>It checks if the size of the range {@code (right - left)}
+     *     is greater than a predefined constant value {@code INTRO_SIZE}.
+     *     If it is, it proceeds with the sorting algorithm.
+     *     Otherwise, it directly calls the {@code insertion} method to perform insertion sort on the smaller range.</li>
+     *     <li>If the size of the range is larger than {@code INTRO_SIZE} and the {@code maxDepth} is <i>0</i>
+     *     (indicating that the recursion depth has reached its maximum allowed value),
+     *     it calls the {@code introSortClass} method to perform a specific sorting operation for the remaining range.</li>
+     *     <li>If the size of the range is larger than {@code INTRO_SIZE} and the {@code maxDepth} is not <i>0</i>,
+     *     it proceeds with the intro sort algorithm.</li>
+     *     <li>It calls the {@code partitionDual} method to select two pivot elements and partition the range into three sub-ranges.</li>
+     *     <li>It recursively calls the {@code introRecursive} method on the left sub-range,
+     *     from {@code left} to {@code (pivots[0] - 1)}, with the {@code maxDepth} reduced by <i>1</i>.</li>
+     *     <li>It recursively calls the {@code introRecursive} method on the middle sub-range,
+     *     from {@code (pivots[0] + 1)} to {@code (pivots[1] - 1)}, with the maxDepth reduced by <i>1</i>.</li>
+     *     <li>It recursively calls the introRecursive method on the right sub-range,
+     *     from {@code pivots[1] + 1} to {@code right}, with the {@code maxDepth} reduced by <i>1</i>.</li>
+     *     <li>The recursion continues until the range is small enough to switch to insertion sort.</li>
+     *     <li>At that point, it calls the {@code insertion} method to perform insertion sort on the remaining range.</li>
+     * </ul>
+     * {@code introRecursive} method recursively applies the intro sort algorithm to sort a given array.
+     * It switches to insertion sort when the sub-array size becomes small enough.
+     * The maximum recursion depth is used to limit the depth of recursion to prevent excessive stack usage.
+     * @param       array The array to be sorted.
+     * @param       left The starting index of the subarray to be sorted.
+     * @param       right The ending index (inclusive) of the subarray to be sorted.
+     * @param       maxDepth The maximum depth or recursion level allowed before switching to another sorting algorithm.
+     * @param       functional lambda expression for comparison.
+     * @see         mz.intro.Intro#INTRO_SIZE
+     * @see         mz.intro.Intro#introSortClass(Comparable[], int, int, SortFunctional)
+     * @see         mz.QuickInterface#partitionDual(Comparable[], int, int, SortFunctional)
+     * @see         mz.intro.Intro#introRecursive(Comparable[], int, int, int, SortFunctional)
+     * @see         mz.InsertionInterface#insertion(Comparable[], int, int, SortFunctional)
+     */
+    @Override
+    default void introRecursive(T[] array, int left, int right, int maxDepth, SortFunctional<T> functional) {
+        if ((right - left) > INTRO_SIZE) {
+            if (maxDepth == 0) {
+                introSortClass(array, left, right, functional);
+            }
+            int[] pivots = partitionDual(array, left, right, functional);
+            introRecursive(array, left, (pivots[0] - 1), (maxDepth - 1), functional);
+            introRecursive(array, (pivots[0] + 1), (pivots[1] - 1), (maxDepth - 1), functional);
+            introRecursive(array, (pivots[1] + 1), right, (maxDepth - 1), functional);
+        } else {
+            insertion(array, left, right, functional);
         }
     }
 }
