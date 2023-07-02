3ec03b2bf968ed17356facb40a0ab83de6d265a4
Magyar Zoltán <118557170+MagyarZoli@users.noreply.github.com>
2023-07-02 14:04:53 +0200

    1.1 update MergeInsertion

--- src/main/java/mz/MergeInsertion.java
+++ src/main/java/mz/MergeInsertion.java
@@ -5,7 +5,7 @@ package mz;
  * It uses fewer comparisons in the worst case than the best previously known algorithms,
  * insertion sort and merge sort, it remains of theoretical interest in connection with
  * the problem of sorting with a minimum number of comparisons.
- * @since       1.0
+ * @since       1.1
  * @author      <a href=https://github.com/MagyarZoli>Magyar Zoltán</a>
  */
 @SuppressWarnings("rawtypes")
@@ -49,7 +49,6 @@ implements InsertionInterface<Comparable>{
      * Average Case Complexity: <em>O(n log(n))</em><br>
      * Auxiliary Space:         <em>O(n)</em><br>
      * Stability:               <b>Yes</b>
-     * @see         mz.intro.introDPQ.IntroDPQMergeInsertion#IntroDPQMergeInsertion() IntroDPQMergeInsertion
      * @see         mz.intro.IntroMergeInsertion#IntroMergeInsertion() IntroMergeInsertion
      */
     public MergeInsertion() {}
@@ -73,6 +72,16 @@ implements InsertionInterface<Comparable>{
     }
 
     /**
+     * {@inheritDoc}
+     * @param       array to be arranged.
+     * @param       functional lambda expression for comparison.
+     */
+    @Override
+    public void sortArrayFun(Comparable[] array, SortFunctional<Comparable> functional) {
+        mergeInsertion(array, functional);
+    }
+
+    /**
      * {@code mergeInsertionInc} is a modified version of the Merge Sort algorithm that incorporates an insertion sort optimization.
      * It sorts an array of Comparable objects by recursively dividing
      * it into smaller subarrays and performing either an insertion sort or a merge sort, depending on the size of the subarray.
@@ -93,8 +102,7 @@ implements InsertionInterface<Comparable>{
      * Insertion sort is generally more efficient than merge sort for small arrays,
      * as it has a lower constant factor and performs fewer comparisons and swaps.
      * @param       array to be arranged.
-     * @see         mz.InsertionInterface#insertionInc(Comparable[], int, int)
-     * @see         mz.Merge#mergingInc(Comparable[], int, int, int)
+     * @see         mz.MergeInsertion#mergeInsertionInc(Comparable[], int, int)
      */
     protected void mergeInsertionInc(Comparable[] array) {
         mergeInsertionInc(array, 0, (array.length - 1));
@@ -121,11 +129,33 @@ implements InsertionInterface<Comparable>{
      * Insertion sort is generally more efficient than merge sort for small arrays,
      * as it has a lower constant factor and performs fewer comparisons and swaps.
      * @param       array to be arranged.
-     * @see         mz.InsertionInterface#insertionInc(Comparable[], int, int)
-     * @see         mz.Merge#mergingInc(Comparable[], int, int, int)
+     * @see         mz.MergeInsertion#mergeInsertionDec(Comparable[], int, int)
      */
     protected void mergeInsertionDec(Comparable[] array) {
         mergeInsertionDec(array, 0, (array.length - 1));
+    }
+
+    /**
+     * {@code mergeInsertion} method being called with the entire {@code array} and the appropriate range.
+     * <ul>
+     *     <li>The method {@code mergeInsertion} is called with {@code array} as the input array,
+     *     <i>0</i> as the left index, and {@code (array.length - 1)} as the right index.
+     *     This ensures that the entire array is sorted using the {@code mergeInsertion} algorithm.</li>
+     *     <li>By passing the complete array to {@code mergeInsertion},
+     *     the method will recursively divide the array into smaller subarrays until
+     *     the subarrays reach the {@code MERGE_THRESHOLD} size.
+     *     At that point, insertion sort will be used to sort the subarrays,
+     *     and then the sorted subarrays will be merged using the merging method.</li>
+     * </ul>
+     * {@code mergeInsertion} algorithm combines the benefits of merge sort and insertion sort to efficiently sort the array.
+     * The merge sort algorithm is used for larger subarrays, while insertion sort is used for smaller subarrays,
+     * resulting in an optimized sorting process.
+     * @param       array to be arranged.
+     * @param       functional lambda expression for comparison.
+     * @see         mz.MergeInsertion#mergeInsertion(Comparable[], int, int, SortFunctional)
+     */
+    protected void mergeInsertion(Comparable[] array, SortFunctional<Comparable> functional) {
+        mergeInsertion(array, 0, (array.length - 1), functional);
     }
 
     /**
@@ -202,6 +232,50 @@ implements InsertionInterface<Comparable>{
                 mergeInsertionDec(array, left, mid);
                 mergeInsertionDec(array, (mid + 1), right);
                 mergingDec(array, left, mid, right);
+            }
+        }
+    }
+
+    /**
+     * {@code mergeInsertion} method takes an array of {@code Comparable} objects,
+     * along with the {@code left} and {@code right} indices specifying the range of elements to be sorted.
+     * It also takes a {@code SortFunctional<Comparable>} object representing the custom comparison logic to be used for sorting.
+     * <ul>
+     *     <li>The method starts with an {@code if} statement to check if the range of elements ({@code left} to {@code right})
+     *     is valid and contains at least one element {@code (left < right)}.</li>
+     *     <li>If the range is valid, the method then checks if the size of
+     *     the range is less than or equal to a predefined {@code MERGE_THRESHOLD} value.
+     *     If so, the range is considered small enough to be sorted using the insertion sort algorithm,
+     *     and the {@code insertion} method is called to sort the range.</li>
+     *     <li>If the size of the range exceeds the {@code MERGE_THRESHOLD}, the method proceeds with the merge sort algorithm.
+     *     It calculates the midpoint of the range using {@code (left + ((right - left) / 2))}.</li>
+     *     <li>The {@code mergeInsertion} method is then recursively called twice, dividing the range into two halves.
+     *     The first recursive call sorts the left half of the range ({@code left} to {@code mid}),
+     *     and the second recursive call sorts the right half of the range ({@code (mid + 1)} to {@code right}),
+     *     both using the same {@code mergeInsertion} method.</li>
+     *     <li>Finally, the merging method is called to {@code merge} the two sorted halves of
+     *     the range ({@code left} to {@code mid} and {@code (mid + 1)} to {@code right}) into a single sorted range.</li>
+     * </ul>
+     * By using the insertion sort optimization for small subarrays, the algorithm aims to improve
+     * the performance of merge sort when dealing with smaller chunks of data.
+     * Insertion sort is generally more efficient than merge sort for small arrays,
+     * as it has a lower constant factor and performs fewer comparisons and swaps.
+     * @param       array to be arranged.
+     * @param       left The starting index of the subarray to be sorted.
+     * @param       right The ending index (exclusive) of the subarray to be sorted.
+     * @param       functional lambda expression for comparison.
+     * @see         mz.InsertionInterface#insertion(Comparable[], int, int, SortFunctional)
+     * @see         mz.Merge#merging(Comparable[], int, int, int, SortFunctional)
+     */
+    protected void mergeInsertion(Comparable[] array, int left, int right, SortFunctional<Comparable> functional) {
+        if (left < right) {
+            if ((right - left) <= MERGE_THRESHOLD) {
+                insertion(array, left, right, functional);
+            } else {
+                int mid = (left + ((right - left) / 2));
+                mergeInsertion(array, left, mid, functional);
+                mergeInsertion(array, (mid + 1), right, functional);
+                merging(array, left, mid, right, functional);
             }
         }
     }
