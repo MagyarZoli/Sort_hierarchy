2f2f9e8a228f2d5634f5b955cf14f5ba7b587bac
Magyar Zoltán <118557170+MagyarZoli@users.noreply.github.com>
2023-07-20 20:52:50 +0200

    1.3 update MergeInPlace

--- src/main/java/mz/MergeInPlace.java
+++ src/main/java/mz/MergeInPlace.java
@@ -1,10 +1,12 @@
 package mz;
 
+import java.util.List;
+
 /**
  * Merge In Place Sort is a variant of the merge sort algorithm that performs the sorting operation directly on the input array,
  * without using any additional arrays for merging.
  * It achieves this by carefully manipulating the elements within the original array during the merging process.
- * @since       1.1
+ * @since       1.3
  * @author      <a href=https://github.com/MagyarZoli>Magyar Zoltán</a>
  */
 @SuppressWarnings("rawtypes")
@@ -69,6 +71,34 @@ extends Merge {
     @Override
     public void sortArrayFun(Comparable[] array, SortFunctional<Comparable> functional) {
         merge(array, functional);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param       list to be arranged.
+     */
+    @Override
+    public void sortListInc(List<? extends Comparable> list) {
+        mergeInc(list);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param       list to be arranged.
+     */
+    @Override
+    public void sortListDec(List<? extends Comparable> list) {
+        mergeDec(list);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param       list to be arranged.
+     * @param       functional lambda expression for comparison.
+     */
+    @Override
+    public void sortListFun(List<? extends Comparable> list, SortFunctional<Comparable> functional) {
+        merge(list, functional);
     }
 
     /**
@@ -255,6 +285,196 @@ extends Merge {
                     index--;
                 }
                 array[left++] = value;
+                mid++;
+                start++;
+            }
+        }
+    }
+
+    /**
+     * {@code mergingInc} is a Java implementation of the merging step in the merge sort algorithm.
+     * It takes a list of {@link java.lang.Comparable Comparable} objects, along with the indices representing
+     * the left, middle, and right boundaries of the sublist being merged.
+     * <ul>
+     *     <li>The {@link java.lang.SuppressWarnings @SuppressWarnings}{@code ("unchecked")} annotation is used to suppress compiler warnings related to type
+     *     safety when using the {@link java.lang.Comparable#compareTo(Object) compareTo} method.</li>
+     *     <li>The method starts by initializing the start variable to {@code (mid + 1)}.
+     *     This is the index where the second sublist begins.</li>
+     *     <li>The code then checks if the element at index {@code mid} is less than or equal to the element at index {@code start}.
+     *     If this condition is true, it means that the two sublists are already sorted, and there is no need to perform any merging.
+     *     In that case, the method simply returns.</li>
+     *     <li>If the condition in step 3 is false, indicating that merging is necessary, a {@code while} loop is used
+     *     to compare elements from both sublists and merge them in sorted order.</li>
+     *     <li>Within the {@code while} loop, there is an {@code if}-{@code else} statement that compares the elements at indices {@code left} and {@code start}.
+     *     If the element at {@code left} is less than or equal to the element at {@code start},
+     *     it means that the element at {@code left} is already in its correct sorted position,
+     *     and we can simply move to the next element in the {@code left} sublist by incrementing the {@code left} variable.</li>
+     *     <li>If the element at {@code left} is greater than the element at {@code start},
+     *     it means that the element at {@code start} needs to be inserted into its correct position in the {@code left} sublist.
+     *     The code creates a temporary variable value to store the value at index {@code start},
+     *     and an index variable {@code index} to keep track of the position where value will be inserted.</li>
+     *     <li>A {@code while} loop is used to shift the elements from {@code index} to {@code left} in the {@code list} one position to the {@code right},
+     *     effectively making space for {@code value} to be inserted at the correct position.</li>
+     *     <li>After shifting the elements, {@code value} is inserted at index {@code left}, and both {@code left}
+     *     and {@code start} are incremented to move to the next elements in their respective sublists.</li>
+     *     <li>The {@code while} loop continues until either the {@code left} sublist or the {@code start} sublist is exhausted, i.e.,
+     *     all elements from either sublist have been merged.</li>
+     * </ul>
+     * {@code mergingInc} this method performs the merging of two sorted sublists within the larger list.
+     * It uses a temporary variable to insert elements from the second sublist into their correct positions within
+     * the first sublist, effectively merging the two sublists into a single sorted sublist.
+     * @param       list to be arranged.
+     * @param       left from the list, index value, must be smaller than the {@code mid} and {@code right} value.
+     * @param       mid from the list, index value, must be smaller than the {@code right} value, and must be greater than the {@code left}.
+     * @param       right from the list, index value, must be greater than the {@code left} and {@code mid} value.
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public <L extends Comparable> void mergingInc(List<L> list, int left, int mid, int right) {
+        int start = (mid + 1);
+        if (list.get(start).compareTo(list.get(mid)) >= 0) {
+            return;
+        }
+        while ((left <= mid) && (start <= right)) {
+            if (list.get(start).compareTo(list.get(left)) >= 0) {
+                left++;
+            } else {
+                L value = list.get(start);
+                int index = start;
+                while (index != left) {
+                    list.set(index, list.get(index - 1));
+                    index--;
+                }
+                list.set(left++, value);
+                mid++;
+                start++;
+            }
+        }
+    }
+
+    /**
+     * {@code mergingDec} is a Java implementation of the merging step in the merge sort algorithm.
+     * It takes an list of {@link java.lang.Comparable Comparable} objects, along with the indices representing
+     * the left, middle, and right boundaries of the sublist being merged.
+     * <ul>
+     *     <li>The {@link java.lang.SuppressWarnings @SuppressWarnings}{@code ("unchecked")} annotation is used to suppress compiler warnings related to type
+     *     safety when using the {@link java.lang.Comparable#compareTo(Object) compareTo} method.</li>
+     *     <li>The method starts by initializing the start variable to {@code (mid + 1)}.
+     *     This is the index where the second sublist begins.</li>
+     *     <li>The code then checks if the element at index {@code mid} is more than or equal to the element at index {@code start}.
+     *     If this condition is true, it means that the two sublists are already sorted, and there is no need to perform any merging.
+     *     In that case, the method simply returns.</li>
+     *     <li>If the condition in step 3 is false, indicating that merging is necessary, a {@code while} loop is used
+     *     to compare elements from both sublists and merge them in sorted order.</li>
+     *     <li>Within the {@code while} loop, there is an {@code if}-{@code else} statement that compares the elements at indices {@code left} and {@code start}.
+     *     If the element at {@code left} is more than or equal to the element at {@code start},
+     *     it means that the element at {@code left} is already in its correct sorted position,
+     *     and we can simply move to the next element in the {@code left} sublist by decrementing the {@code left} variable.</li>
+     *     <li>If the element at {@code left} is smaller than the element at {@code start},
+     *     it means that the element at {@code start} needs to be inserted into its correct position in the {@code left} sublist.
+     *     The code creates a temporary variable value to store the value at index {@code start},
+     *     and an index variable {@code index} to keep track of the position where value will be inserted.</li>
+     *     <li>A {@code while} loop is used to shift the elements from {@code index} to {@code left} in the {@code list} one position to the {@code right},
+     *     effectively making space for {@code value} to be inserted at the correct position.</li>
+     *     <li>After shifting the elements, {@code value} is inserted at index {@code left}, and both {@code left}
+     *     and {@code start} are decremented to move to the next elements in their respective sublists.</li>
+     *     <li>The {@code while} loop continues until either the {@code left} sublist or the {@code start} sublist is exhausted, i.e.,
+     *     all elements from either sublist have been merged.</li>
+     * </ul>
+     * {@code mergingDec} this method performs the merging of two sorted sublists within the larger list.
+     * It uses a temporary variable to insert elements from the second sublist into their correct positions within
+     * the first sublist, effectively merging the two sublists into a single sorted sublist.
+     * @param       list to be arranged.
+     * @param       left from the list, index value, must be smaller than the {@code mid} and {@code right} value.
+     * @param       mid from the list, index value, must be smaller than the {@code right} value, and must be greater than the {@code left}.
+     * @param       right from the list, index value, must be greater than the {@code left} and {@code mid} value.
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public <L extends Comparable> void mergingDec(List<L> list, int left, int mid, int right) {
+        int start = (mid + 1);
+        if (list.get(start).compareTo(list.get(mid)) <= 0) {
+            return;
+        }
+        while ((left <= mid) && (start <= right)) {
+            if (list.get(start).compareTo(list.get(left)) <= 0) {
+                left++;
+            } else {
+                L value = list.get(start);
+                int index = start;
+                while (index != left) {
+                    list.set(index, list.get(index - 1));
+                    index--;
+                }
+                list.set(left++, value);
+                mid++;
+                start++;
+            }
+        }
+    }
+
+    /**
+     * {@code merging} method being overridden.
+     * This method is responsible for merging two sorted sublists
+     * {@code (list.set(left) to list.set(mid) and list.set(mid + 1) to list.set(right))} into a single sorted sublist.
+     * <ul>
+     *     <li>It initializes the {@code start} variable as {@code (mid + 1)},
+     *     which represents the starting index of the second sublist.</li>
+     *     <li>It creates a new {@code SortFunctional<Comparable>} object called {@code functionalAddEquals}
+     *     by calling the method {@code functionalComparableToAddEquals(functional)}.
+     *     The purpose of this new functional interface.</li>
+     *     <li>It checks if the first element of the second sublist {@code list.set(start)} is greater than or equal to
+     *     the last element of the first sublist {@code list.set(mid)}
+     *     by using the {@code functionalAddEquals} functional interface.
+     *     If this condition is {@code true}, it means that the two sublists are already in order,
+     *     and the method returns without performing any further merging.</li>
+     *     <li>It enters a {@code while} loop that continues until either
+     *     the first sublist or the second sublist is completely traversed.</li>
+     *     Inside the {@code while} loop, it compares the elements at {@code list.set(start)} and {@code list.set(left)}.
+     *     If the element at {@code list.set(start)} is greater than or equal to the element at {@code list.set(left)},
+     *     it means that the next element in the merged sublist should come from the first sublist.
+     *     In this case, it increments the {@code left} index to move to the next element in the first sublist.
+     *     <li>If the element at {@code list.set(start)} is smaller than the element at {@code list.set(left)}, it means that
+     *     the next element in the merged sublist should come from the second sublist.
+     *     In this case, it performs a series of element shifts in
+     *     the first sublist to make space for the element at {@code list.set(start)}.
+     *     It starts from the current {@code start} index and moves backward,
+     *     shifting each element one position to the right until it reaches the left index.
+     *     Finally, it assigns the element at {@code list.get(start)} to the {@code left} index.</li>
+     *     <li>After each element is processed, it increments {@code left} to move to the next position in the first sublist,
+     *     increments {@code mid} to account for the element insertion,
+     *     and increments {@code start} to move to the next position in the second sublist.</li>
+     *     <li>Once the while loop exits, the merging is complete, and the two sublists are merged into a single sorted sublist.</li>
+     * </ul>
+     * {@code merging} this method performs the merging of two sorted sublists within the larger list.
+     * It uses a temporary variable to insert elements from the second sublist into their correct positions within
+     * the first sublist, effectively merging the two sublists into a single sorted sublist.
+     * @param       list to be arranged.
+     * @param       left from the list, index value, must be smaller than the {@code mid} and {@code right} value.
+     * @param       mid from the list, index value, must be smaller than the {@code right} value, and must be greater than the {@code left}.
+     * @param       right from the list, index value, must be greater than the {@code left} and {@code mid} value.
+     * @param       functional lambda expression for comparison.
+     * @see         Sort#functionalComparableToAddEquals(SortFunctional)
+     * @see         SortFunctional#functionalCompareTo(Comparable, Comparable)
+     */
+    @Override
+    public <L extends Comparable> void merging(List<L> list, int left, int mid, int right, SortFunctional<Comparable> functional) {
+        int start = (mid + 1);
+        SortFunctional<Comparable> functionalAddEquals = functionalComparableToAddEquals(functional);
+        if (functionalAddEquals.functionalCompareTo(list.get(start), list.get(mid))) {
+            return;
+        }
+        while ((left <= mid) && (start <= right)) {
+            if (functionalAddEquals.functionalCompareTo(list.get(start), list.get(left))) {
+                left++;
+            } else {
+                L value = list.get(start);
+                int index = start;
+                while (index != left) {
+                    list.set(index, list.get(index - 1));
+                    index--;
+                }
+                list.set(left++, value);
                 mid++;
                 start++;
             }
