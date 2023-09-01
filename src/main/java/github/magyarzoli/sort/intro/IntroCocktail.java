c1a03f8c3c228cdc6ca94c4c1333572eff14803d
Magyar Zoltán <118557170+MagyarZoli@users.noreply.github.com>
2023-09-01 10:10:34 +0200

    1.3 update IntroCocktail

--- src/main/java/github/magyarzoli/sort/intro/IntroCocktail.java
+++ src/main/java/github/magyarzoli/sort/intro/IntroCocktail.java
@@ -1,5 +1,7 @@
 package github.magyarzoli.sort.intro;
 
+import java.util.List;
+
 import github.magyarzoli.SortFunctional;
 import github.magyarzoli.sort.Cocktail;
 
@@ -11,7 +13,7 @@ import github.magyarzoli.sort.Cocktail;
  * To mitigate the risk of Quick Sort's worst-case behavior, Intro Sort monitors the recursion depth during the sorting process.
  * If the depth exceeds a certain threshold, the algorithm switches to
  * Another Sort is Cocktail Sort.
- * @since       1.1
+ * @since       1.3
  * @author      <a href=https://github.com/MagyarZoli>Magyar Zoltán</a>
  */
 @SuppressWarnings("rawtypes")
@@ -90,6 +92,34 @@ public class IntroCocktail
 
     /**
      * {@inheritDoc}
+     * @param       list to be arranged.
+     */
+    @Override
+    public void sortListInc(List<? extends Comparable> list) {
+        introInc(list);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param       list to be arranged.
+     */
+    @Override
+    public void sortListDec(List<? extends Comparable> list) {
+        introDec(list);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param       list to be arranged.
+     * @param       functional lambda expression for comparison.
+     */
+    @Override
+    public void sortListFun(List<? extends Comparable> list, SortFunctional<Comparable> functional) {
+        intro(list, functional);
+    }
+
+    /**
+     * {@inheritDoc}
      * @param       array The array to be sorted.
      * @param       left The starting index of the subarray to be sorted.
      * @param       right The ending index (inclusive) of the subarray to be sorted.
@@ -120,5 +150,39 @@ public class IntroCocktail
     @Override
     public void introSortClass(Comparable[] array, int left, int right, SortFunctional<Comparable> functional) {
         cocktail(array, left, right, functional);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param       list The array to be sorted.
+     * @param       left The starting index of the sublist to be sorted.
+     * @param       right The ending index (inclusive) of the sublist to be sorted.
+     */
+    @Override
+    public <L extends Comparable> void introSortClassInc(List<L> list, int left, int right) {
+        cocktailInc(list, left, right);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param       list The array to be sorted.
+     * @param       left The starting index of the sublist to be sorted.
+     * @param       right The ending index (inclusive) of the sublist to be sorted.
+     */
+    @Override
+    public <L extends Comparable> void introSortClassDec(List<L> list, int left, int right) {
+        cocktailDec(list, left, right);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @param       list The array to be sorted.
+     * @param       left The starting index of the sublist to be sorted.
+     * @param       right The ending index (inclusive) of the sublist to be sorted.
+     * @param       functional lambda expression for comparison.
+     */
+    @Override
+    public <L extends Comparable> void introSortClass(List<L> list, int left, int right, SortFunctional<Comparable> functional) {
+        cocktail(list, left, right, functional);
     }
 }
